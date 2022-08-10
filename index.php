09cccc1791b81be8a3ea386854446aeb1582c1e0
Alec Yalcin <alec.yalcin@escolar.ifrn.edu.br>
2022-08-10 16:28:51 -0300

    Alteração de index.html -> index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+    class MyDB extends SQLite3 {
+        function __construct() {
+            $this->open('database.db');
+        }
+    }
+    $db = new MyDB();
+    if(!$db) {
+        echo $db->lastErrorMsg();
+    } else {
+        echo "Opened!";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
